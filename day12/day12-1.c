ace5bfa79cf7c1f3d085a876dd7ceb8b54558aec
MS-dino <61901291+BMtdino@users.noreply.github.com>
2024-12-01 22:47:19 +0900

    twenty-first commit

--- day12/day12-1.c
+++ day12/day12-1.c
@@ -2,14 +2,15 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define MAX_TOPPING 100000
-// topping_len은 배열 topping의 길이입니다.
+#define MAX_TOPPING 100000 //토핑 최대 길이
+
 int solution(int topping[], size_t topping_len) {
     int answer = 0;
-    int left_counter[MAX_TOPPING + 1] = { 0 };
-    int right_counter[MAX_TOPPING + 1] = { 0 };
-    int left_TC = 0, right_TC = 0;
+    int left_counter[MAX_TOPPING + 1] = { 0 }; // 토핑 왼쪽부터
+    int right_counter[MAX_TOPPING + 1] = { 0 }; // 토핑 오른쪽 부터
+    int left_TC = 0, right_TC = 0; // 좌우 토핑 카운터
 
+    //오른쪽 생성
     for (int i = 0; i < topping_len; i++) {
         if (right_counter[topping[i]] == 0) {
             right_TC++;
@@ -17,6 +18,7 @@ int solution(int topping[], size_t topping_len) {
         right_counter[topping[i]]++;
     }
 
+    //좌우 비교
     for (int i = 0; i < topping_len; i++) {
         if (left_counter[topping[i]] == 0) {
             left_TC++;
@@ -36,18 +38,20 @@ int solution(int topping[], size_t topping_len) {
 }
 
 int main() {
+    // 토핑 구성
     int topping_1[] = {1, 2, 1, 3, 1, 4, 1, 2};
     int topping_2[] = {1, 2, 3, 1, 4};
 
+    //구성된 토핑의 길이
     int topping_1_len = sizeof(topping_1) / sizeof(topping_1[0]);
     int topping_2_len = sizeof(topping_2) / sizeof(topping_2[0]);
 
+    // 결과 저장
     int result_1 = solution(topping_1, topping_1_len);
     int result_2 = solution(topping_2, topping_2_len);
 
-    //printf("[%d]\n", topping_1);
+    //출력
     printf("%d\n", result_1);
-    //printf("[%d]\n", topping_2);
     printf("%d\n", result_2);
 
 }
